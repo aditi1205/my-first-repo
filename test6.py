f39af9a712029174cf064085f43ff3a6e8b1cb50
Aditi Shukla <shuklaaditi1221@gmail.com>
2021-02-28 20:57:02 +0530

    test

---
+++ test6.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 28 20:54:41 2021
+
+@author: hp
+"""
+
+celsius = 37.5
+
+# calculate fahrenheit
+fahrenheit = (celsius * 1.8) + 32
+print('%0.1f degree Celsius is equal to %0.1f degree Fahrenheit' %(celsius,fahrenheit))
